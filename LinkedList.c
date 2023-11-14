189a82c7f4d9abbfa3d5774f2e0abea407bc1e41
M Arjun Krishna <119443675+ArjunKrish7356@users.noreply.github.com>
2023-11-14 10:51:18 +0000

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 
 struct node{
     int data;
     struct node *next;
 };
 
-struct *node Create(int value){
-    struct node newnode = malloc(sizeof(struct node));
+struct node* Create(int value){
+    struct node* newnode = malloc(sizeof(struct node));
     newnode->data=value;
     newnode->next=NULL;
     return newnode;
@@ -14,9 +16,28 @@ struct *node Create(int value){
 
 void addnode(int value,struct node **head){
     struct node* newnode=Create(value);
-    struct node* current=*head;
+    struct node* current=head;
     while((current->next)!=NULL)
         current=current->next;
-    current->next=newnoed;
+    current->next=newnode;
 }
+
+void main(){
+   int n,*head,*current;
+   while(n!=5){
+     printf("Enter you operation: ");
+     scanf("%d",&n);
+     if(n==1){
+        addnode(5,&head);
+     }
+     else if (n==2){
+        current=head;
+        while((current->next)!=NULL){
+            printf("%d",current->data);
+            current=current->next;
+        }
+     }
+   }
+}
+
 
